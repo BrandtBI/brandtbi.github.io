1c97f9dd76e8f58f43e84fa7eb6e14c93981a6ca
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-10 13:28:46 -0500

    Update openweather.js

--- openweather.js
+++ openweather.js
@@ -23,19 +23,26 @@
 
 	myConnector.getData = function(table, doneCallback) {
 	//var tableData = [];	
-	$.getJSON("http://api.openweathermap.org/data/2.5/forecast?lat=44.3668&lon=100.3538&units=imperial&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
-        var list = data.list;
-        var city = data.city;
+	$.getJSON("http://api.openweathermap.org/data/2.5/forecast?lat=44.3668&lon=100.3538&units=imperial&appid=0bed9dddd956dff3252a42b41eccad89", function(resp) {
+        //var list = data.list;
+        //var city = data.city;
+        var feat = resp.features,
         tableData = [];
     //for each result write entry
         for (var i = 0; i < list.length; i++) {
         tableData.push({
-            "city_id":city[i].city.id,
-            "city_name":city[i].city.name,
-            "city_coord_lat":city[i].coord.lat,
-            "city_coord_lon":city[i].coord.lon,
-            "list.dt":list[i].dt,
-            "list_main_temp":list[i].main.temp
+            "city_id":feat[i].id,
+            "city_name":feat[i].name,
+            "city_coord_lat":feat[i].coord.lat,
+            "city_coord_lon":feat[i].coord.lon,
+            "list.dt":feat[i].dt,
+            "list_main_temp":feat[i].main.temp
+            // "city_id":city[i].city.id,
+            // "city_name":city[i].city.name,
+            // "city_coord_lat":city[i].coord.lat,
+            // "city_coord_lon":city[i].coord.lon,
+            // "list.dt":list[i].dt,
+            // "list_main_temp":list[i].main.temp
         });
         }    
     
