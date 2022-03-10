2e5789d33af49f779e39d8b1de07ccc030c64572
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-10 13:21:51 -0500

    Update openweather.js

--- openweather.js
+++ openweather.js
@@ -1,15 +1,7 @@
 (function () {
 
     var myConnector = tableau.makeConnector();
-	myConnector.init = function(initCallback) {
-	initCallback();
-	tableau.submit();
-	};
-
-    myConnector.getSchema = function (schemaCallback) {
-
-    };
-
+	
 	myConnector.getSchema = function (schemaCallback) {
 	var cols = [
         { id : "city_id", alias : "City ID", columnRole: "dimension", dataType : tableau.dataTypeEnum.string},
@@ -17,17 +9,8 @@
         { id : "city_coord.lat", alias : "Latitude", columnRole: "dimension", dataType : tableau.dataTypeEnum.geometry},
         { id : "city_coord_lon", alias : "Longitude", columnRole: "dimension", dataType : tableau.dataTypeEnum.geometry},
         { id : "list_dt", alias : "Time of Data", columnRole: "dimension", dataType : tableau.dataTypeEnum.datetime},
-        { id : "list_main_temp", alias : "Temperature", columnRole: "dimension", dataType : tableau.dataTypeEnum.float},
-        
-		// { id : "CITY_ID", alias : "CITY_ID", columnRole: "dimension", dataType : tableau.dataTypeEnum.string },
-		// { id : "DATE", alias : "DATE", dataType : tableau.dataTypeEnum.date },
-		// { id : "CITY", alias : "CITY", columnRole: "dimension", dataType : tableau.dataTypeEnum.string },
-		// { id : "DESCRIPTION", alias : "DESCRIPTION", columnRole: "dimension", dataType : tableau.dataTypeEnum.string },
-		// { id : "ICON", alias : "ICON", columnRole: "dimension", dataType : tableau.dataTypeEnum.string },
-		// { id : "CLOUD", alias : "CLOUD", columnRole: "measure", dataType : tableau.dataTypeEnum.float },
-		// { id : "DEGREES", alias : "DEGREES", dataType : tableau.dataTypeEnum.float },
-		// { id : "MAIN", alias : "MAIN", dataType : tableau.dataTypeEnum.string }
-		];
+        { id : "list_main_temp", alias : "Temperature", columnRole: "dimension", dataType : tableau.dataTypeEnum.float}
+    ];
 
 	var tableInfo = {
 		id : "WeatherFeed",
@@ -47,78 +30,21 @@
                 //for each result write entry
         for (i = 0; i < list.length; i++) {
         tableData.push({
-            "city_id":city[i]["City ID"],
-            "city_name":city[i]["City Name"],
-            "city_coord_lat":city[i]["Latitude"],
-            "city_coord_lon":city[i]["Longitude"],
-            "list.dt":list[i]["Time of Date"],
-            "list_main_temp":list[i]["Temperature"]
-            
-            // "CITY_ID": city.id,
-            // "CITY" : city.name,
-            // 'DATE': new Date(list[i].dt * 1000),
-            // 'MAIN': list[i].weather[0].main,
-            // 'DESCRIPTION': list[i].weather[0].description,
-            // 'ICON': list[i].weather[0].icon,
-            // 'DEGREES': list[i].deg,
-            // 'CLOUD': list[i].clouds
-            });
+            "city_id":city[i].city.id,
+            "city_name":city[i].city.name,
+            "city_coord_lat":city[i].coord.lat,
+            "city_coord_lon":city[i].coord.lon,
+            "list.dt":list[i].dt,
+            "list_main_temp":list[i].main.temp
+        });
         }    
     
-    
-    // var list = data.list,
-        //     tableData = [];
-		// 	var city = data.city;
-		// 	//for each result write entry
-		// 	for (i = 0; i < list.length; i++) {
-        //     tableData.push({
-        //         "city_id":list[i].city.id,
-        //         "city_name":list[i].city.name,
-        //         "city_coord_lat":list[i].city.coord.lat,
-        //         "city_coord_lon":list[i].city.coord.lon,
-        //         "list.dt":list[i].dt,
-        //         "list_main_temp":list[i].main.temp
-                
-        //         // "CITY_ID": city.id,
-		// 		// "CITY" : city.name,
-		// 		// 'DATE': new Date(list[i].dt * 1000),
-		// 		// 'MAIN': list[i].weather[0].main,
-		// 		// 'DESCRIPTION': list[i].weather[0].description,
-		// 		// 'ICON': list[i].weather[0].icon,
-		// 		// 'DEGREES': list[i].deg,
-		// 		// 'CLOUD': list[i].clouds
-		// 		});
-		// 	}
-
-			table.appendRows(tableData);
-			
+    	table.appendRows(tableData);
+			doneCallback();
 		});
-
-	// //Baltimore	
-    // $.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily/?id=4347778&units=imperial&appid=09bf011b56073de6cff36d95fcc99f44", function(data) {
-    //     var list = data.list,
-    //         tableData = [];
-	// 		var city = data.city;
-	// 		//for each result write entry
-	// 		for (i = 0; i < list.length; i++) {
-    //         tableData.push({
-    //             "CITY_ID": city.id,
-	// 			"CITY" : city.name,
-	// 			'DATE': new Date(list[i].dt * 1000),
-	// 			'MAIN': list[i].weather[0].main,
-	// 			'DESCRIPTION': list[i].weather[0].description,
-	// 			'ICON': list[i].weather[0].icon,
-	// 			'DEGREES': list[i].deg,
-	// 			'CLOUD': list[i].clouds
-	// 			});
-	// 		}
-
-	// 		table.appendRows(tableData);
-	// 		//doneCallback();
-    //    });	
-        doneCallback();
 	};
 	
+
     tableau.registerConnector(myConnector);
 	
 	$(document).ready(function () {
@@ -128,4 +54,5 @@
     });
 });
 })();
+
         
