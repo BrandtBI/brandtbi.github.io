7b869e0a730a8615baa934e3c70b5656eca59146
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-10 20:21:26 -0500

    Update openweather.js

--- openweather.js
+++ openweather.js
@@ -23,40 +23,34 @@
 
 	myConnector.getData = function(table, doneCallback) {
 	//var tableData = [];	
-	$.getJSON("http://api.openweathermap.org/data/2.5/forecast?lat=44.3668&lon=100.3538&units=imperial&appid=0bed9dddd956dff3252a42b41eccad89", function(resp) {
-      //  var list = data.list;
-      //  var city = data.city;
-        var feat = resp.features,
+	$.getJSON("http://api.openweathermap.org/data/2.5/forecast?lat=44.3668&lon=100.3538&units=imperial&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
+        var list = data.list;
+        var city = data.city;
+       // var feat = resp.features,
         tableData = [];
       //for each result write entry
-        for (var i = 0, len = feat.length; i < len; i++) {
-      // for (i = 0; i < list.length; i++) {
+      //  for (var i = 0, len = feat.length; i < len; i++) {
+       for (i = 0; i < list.length; i++) {
        tableData.push({
-            // "city_id":feat[i]["City ID"],
-            // "city_name":feat[i]["City Name"],
-            // "city_coord_lat":feat[i]["Latitude"],
-            // "city_coord_lon":feat[i]["Longitude"],
-            // "list_dt":feat[i]["Time of Date"],
-            // "list_main_temp":feat[i]["Temperature"]
-            "id":feat[i].city.id,
-            "name":feat[i].city.name,
-            "lat":feat[i].city.coord.lat,
-            "lon":feat[i].city.coord.lon,
-            "dt":feat[i].list.dt,
-            "temp":feat[i].list.main.temp
-            // "city_id":feat[i]["City ID"],
-            // "city_name":feat[i]["City Name"],
-            // "city_coord_lat":feat[i]["Latitude"],
-            // "city_coord_lon":feat[i]["Longitude"],
-            // "list_dt":feat[i]["Time of Data"],
-            // "list_main_temp":feat[i]["Temperature"]
-            
-            // "city_id":city[i].id,
-            // "city_name":city[i].name,
-            // "city_coord_lat":city[i].coord.lat,
-            // "city_coord_lon":city[i].coord.lon,
-            // "list_dt":list[i].dt,
-            // "list_main_temp":list[i].main.temp
+            "id":city.id,
+            "name":city.name,
+            "lat":city.coord.lat,
+            "lon":city.coord.lon,
+            "dt":list[i].list.dt,
+            "temp":list[i].list.main.temp
+            // "id":feat[i].city.id,
+            // "name":feat[i].city.name,
+            // "lat":feat[i].city.coord.lat,
+            // "lon":feat[i].city.coord.lon,
+            // "dt":feat[i].list.dt,
+            // "temp":feat[i].list.main.temp
+            // "id":city[i]["City ID"],
+            // "name":city[i]["City Name"],
+            // "lat":city[i]["Latitude"],
+            // "lon":city[i]["Longitude"],
+            // "dt":list[i]["Time of Data"],
+            // "temp":list[i]["Temperature"]
+
         });
         }    
     
