eef7e32a655167545dce66babcbb3c3c2e5afc10
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-10 20:48:25 -0500

    Update openweather.js

--- openweather.js
+++ openweather.js
@@ -4,12 +4,12 @@
 	
 	myConnector.getSchema = function (schemaCallback) {
 	var cols = [
-        { id : "id", alias : "City ID",  dataType : tableau.dataTypeEnum.string}//,
+        { id : "id", alias : "City ID",  dataType : tableau.dataTypeEnum.string},
         // { id : "name", alias : "City Name",  dataType : tableau.dataTypeEnum.string},
         // { id : "lat", alias : "Latitude",  dataType : tableau.dataTypeEnum.float},
         // { id : "lon", alias : "Longitude",  dataType : tableau.dataTypeEnum.float},
         // { id : "dt", alias : "Time of Data",  dataType : tableau.dataTypeEnum.datetime},
-        // { id : "temp", alias : "Temperature",  dataType : tableau.dataTypeEnum.float}
+         { id : "temp", alias : "Temperature",  dataType : tableau.dataTypeEnum.float}
     ];
 
 	var tableInfo = {
@@ -32,12 +32,12 @@
         for (var i = 0, len = feat.length; i < len; i++) {
       // for (var i = 0, len = list.length; i < len; i++) {
        tableData.push({
-            "id":feat[i].city.id//,
+            "id":city.id,
             // "name":city.name,
             // "lat":city.coord.lat,
             // "lon":city.coord.lon,
             // "dt":list[i].dt,
-            // "temp":list[i].main[0].temp
+             "temp":feat[i].list.main.temp
             
             // "id":feat[i].city.id,
             // "name":feat[i].city.name,
