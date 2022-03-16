de118a623d88901d6a24aab4a67ee78f27eca445
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-15 21:33:31 -0400

    openweather.js update

--- openweather.js
+++ openweather.js
@@ -1,82 +1,3 @@
-// (function () {
-
-//     var myConnector = tableau.makeConnector();
-	
-// 	myConnector.getSchema = function (schemaCallback) {
-       
-//         var cols = [
-//         { id : "city", alias : "City", dataType : tableau.dataTypeEnum.string},
-//         { id : "lat", alias : "Latitude",  dataType : tableau.dataTypeEnum.float},
-//         { id : "lon", alias : "Longitude",  dataType : tableau.dataTypeEnum.float},
-//         { id : "dt", alias : "Time Forecasted",  dataType : tableau.dataTypeEnum.datetime},
-//         { id : "day", alias : "Temperature",  dataType : tableau.dataTypeEnum.float},
-//         { id : "min", alias : "Min Temp",  dataType : tableau.dataTypeEnum.datetime},
-//         { id : "max", alias : "Max Temp",  dataType : tableau.dataTypeEnum.float},
-//         { id : "moonPhase", alias : "Moon Phase",  dataType : tableau.dataTypeEnum.float},
-//         { id : "humidity", alias : "Humidity", dataType : tableau.dataTypeEnum.float},
-//         { id : "windSpeed", alias : "Wind Speed", dataType : tableau.dataTypeEnum.float},
-//         { id : "pop", alias : "Rain Probability", dataType : tableau.dataTypeEnum.float},
-//         { id : "rain", alias : "Rain Volume", dataType : tableau.dataTypeEnum.float},
-//         { id : "snow", alias : "Snow Volume", dataType : tableau.dataTypeEnum.float},
-//         { id : "description", alias : "Weather Description", dataType : tableau.dataTypeEnum.string},
-//         { id : "icon", alias : "Icon", dataType : tableau.dataTypeEnum.string}
-//        ];
-
-// 	var tableSchema = {
-// 		id : "aberdeen",
-// 		alias : "5 Day Weather Forecast",
-// 		columns : cols
-// 		};
-    
-// 	schemaCallback([tableSchema]);
-// 	};
-
-// 	myConnector.getData = function(table, doneCallback) {
-//         let lat = -98.486481;
-//         let lon = 45.464699;
-//         let city = "aberdeen";
-// 	$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=44.36832&lon=-100.350967&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
-//        // var city = data.city;
-//         var daily = data.daily,
-//        // var feat = resp.features,
-//         tableData = [];
-//       //for each result write entry
-//       //  for (var i = 0, len = feat.length; i < len; i++) {
-//        for (var i = 0, len = daily.length; i < len; i++) {
-//        tableData.push({
-            
-//             "city":city,
-//             "lat":lat,
-//             "lon":lon,    
-//             "dt":daily[i].dt, 
-//             "temp":daily[i].temp.day,
-//             "min":daily[i].temp.min,
-//             "max":daily[i].temp.max,
-//             "moonPhase":daily[i].moon_phase,
-//             "windSpeed":daily[i].wind_speed,
-//             "description":daily[i].weather.description,
-//             "icon":daily[i].weather.icon
-            
-//         });
-//         }    
-    
-//     	table.appendRows(tableData);
-// 		doneCallback();
-// 		});
-// 	};
-	
-
-//     tableau.registerConnector(myConnector);
-// })();
-
-// 	$(document).ready(function () {
-//     $("#submitButton").click(function () {
-//         tableau.connectionName = "Aberdeen";
-//         tableau.submit();
-//     });
-// });
-
-
 (function () {
 
     var myConnector = tableau.makeConnector();
@@ -102,24 +23,20 @@
        ];
 
 	var tableSchema = {
-		id : "aberdeen",
+		id : "daily",
 		alias : "5 Day Weather Forecast",
 		columns : cols
 		};
     
 	schemaCallback([tableSchema]);
 	};
-
+/////////// ABERDEEN
 	myConnector.getData = function(table, doneCallback) {
-        // let lat =[-98.486481, -103.598808];
-        // let lon = [45.464699, 43.766651];
-        // let city = ["aberdeen", "custer"];
-        var  location = ["lat=-98.486481&lon=-103.598808","lat=-103.598808&lon=43.766651"];
-        for (i=0; i< location.length; ++i){
-      
-        $.getJSON("https://api.openweathermap.org/data/2.5/onecall?"+location+"&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
-           
-        // var city = data.city;
+        let lat = -98.486481;
+        let lon = 45.464699;
+        let city = "aberdeen";
+	$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=44.36832&lon=-100.350967&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
+       // var city = data.city;
         var daily = data.daily,
        // var feat = resp.features,
         tableData = [];
@@ -141,14 +58,46 @@
             "icon":daily[i].weather.icon
             
         });
-    
         }    
     
     	table.appendRows(tableData);
 		doneCallback();
-         });
-        };
-        
+		});
+    };
+    
+///////// CUSTER
+    myConnector.getData = function(table, doneCallback) {
+        let lon = -103.598808;
+        let lat = -43.766651;
+        let city = "custer";
+	$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=44.36832&lon=-100.350967&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
+       // var city = data.city;
+        var daily = data.daily,
+       // var feat = resp.features,
+        tableData = [];
+      //for each result write entry
+      //  for (var i = 0, len = feat.length; i < len; i++) {
+       for (var i = 0, len = daily.length; i < len; i++) {
+       tableData.push({
+            
+            "city":city,
+            "lat":lat,
+            "lon":lon,    
+            "dt":daily[i].dt, 
+            "temp":daily[i].temp.day,
+            "min":daily[i].temp.min,
+            "max":daily[i].temp.max,
+            "moonPhase":daily[i].moon_phase,
+            "windSpeed":daily[i].wind_speed,
+            "description":daily[i].weather.description,
+            "icon":daily[i].weather.icon
+            
+        });
+        }    
+    
+    	table.appendRows(tableData);
+		doneCallback();
+		});
 	};
 	
 
@@ -157,9 +106,8 @@
 
 	$(document).ready(function () {
     $("#submitButton").click(function () {
-        tableau.connectionName = "Aberdeen";
+        tableau.connectionName = "Daily Weather";
         tableau.submit();
     });
 });
-
 
