997968c5f999d4671fbb2577feb8f0b0bc25e409
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-15 22:47:18 -0400

    Update openweather.js

--- openweather.js
+++ openweather.js
@@ -167,12 +167,12 @@
     };
 
 	myConnector.getData = function(table, doneCallback) {
-    
     var tableData=[];
+    
     /////////// ABERDEEN
-        let lat = -98.486481;
-        let lon = 45.464699;
-        let city = "aberdeen";
+        var lat = -98.486481;
+        var lon = 45.464699;
+        var city = "aberdeen";
 	$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=44.36832&lon=-100.350967&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
        // var city = data.city;
         var daily = data.daily,
@@ -205,9 +205,9 @@
 
 
 ///////// CUSTER
-        let lon = -103.598808;
-        let lat = -43.766651;
-        let city = "custer";
+        var lon = -103.598808;
+        var lat = -43.766651;
+        var city = "custer";
 	$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=44.36832&lon=-100.350967&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
        // var city = data.city;
         var daily = data.daily,
