17a71192f8ccf4cd09ddd4f676b23db00f1df452
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-22 09:56:03 -0400

    Update openweather.js

--- openweather.js
+++ openweather.js
@@ -52,8 +52,8 @@
 //////////// ABERDEEN /////
         
 	$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=44.36832&lon=-100.350967&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89", function(data) {
-       var id = 1;
-       var x = lastid++;
+       var id = 1++;
+       //var x = lastid++;
        var lat = -98.486481;
        var lon = 45.464699;
        var city = "Aberdeen";
@@ -63,7 +63,7 @@
       //for each result write entry
        for (var i = 0, len = daily.length; i < len; i++) {
        tableData.push({
-            "id":[x].id,
+            "id":id,
             "city":city,
             "zip":zip,
             "lat":lat,
