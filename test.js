9191acff7dc8bca76291e9ba644147c0193d66c0
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-22 09:36:56 -0400

    openweather.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 https://api.openweathermap.org/data/2.5/onecall?id=5767918&exclude=minutely,hourly,alerts&units=imperial&lang=en&appid=0bed9dddd956dff3252a42b41eccad89
 
-https://api.openweathermap.org/data/2.5/onecall?lat=%27-98.486481,-103.598808%27&lon=%2745.464699,43.766651%27&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89
+https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=%27-98.486481,-103.598808%27&lon=%2745.464699,43.766651%27&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89
 
 https://api.openweathermap.org/data/2.5/onecall?%27lat=-98.486481&lon=-103.598808,lat=-103.598808&lon=43.766651%27&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89
 
-https://api.openweathermap.org/data/2.5/onecall?lat=-98.486481&lon=-103.598808,lat=-103.598808&lon=43.766651&units=imperial&exclude=current,minutely,hourly,alerts&lang=en&appid=0bed9dddd956dff3252a42b41eccad89
+&dt=1647748800&units=imperial&exclude=hourly&lang=en&appid=0bed9dddd956dff3252a42b41eccad89
