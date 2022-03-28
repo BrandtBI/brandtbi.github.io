fe674857cc09e365ef9052e2e520eaa1afd883b2
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-28 18:37:11 -0400

    incontact

--- hardcode_incontact.js
+++ hardcode_incontact.js
@@ -32,13 +32,13 @@
     myConnector.getData = function(table, doneCallback) {   
         
         $.ajax({
-            method:"GET",    
+            type:"GET",    
             //url: "https://api-c14.incontact.com/inContactAPI/services/v23.0/contacts/completed?startDate=2022-03-22&endDate=2022-03-23&fields=contactID%2CmasterContactID",
             dataType:"json",
             //data: data,
             beforeSend:function(xhr){
-                xhr.setRequestHeader('Accept','application/json');
-                xhr.setRequestHeader('Authorization','Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpY1NQSWQiOjYzLCJpY0FnZW50SWQiOjIwNzg2NDA1LCJzdWIiOiJ1c2VyOjIwNzg2NDA1IiwiaXNzIjoiaHR0cHM6Ly9hcGkuaW5jb250YWN0LmNvbSIsImljQlVJZCI6NDU5NTk2NCwiZ2l2ZW5fbmFtZSI6IlN0ZXBoYW5pZSIsImF1ZCI6IkRldmVsb3BlclBvcnRhbEBOSUNFaW5Db250YWN0IEluYy4iLCJuYW1lIjoic3RlcGhhbmllLmRhdmlzQGJyYW5kdGluZm8uY29tIiwidGVuYW50SWQiOiIxMWU5ZWJlNC05YmNiLThiOWUtODEwNy0wMDUwNTZhMTQwNzQiLCJmYW1pbHlfbmFtZSI6IkRhdmlzIiwiaWNDbHVzdGVySWQiOiJDMTQiLCJuYmYiOjE2NDg0Nzc0NjEsImljU2NvcGUiOiIxLDIsNCw1LDYsNyw4LDEwLDEyIiwiaWF0IjoxNjQ4NDc3NDYxLCJleHAiOjE2NDg0ODEwNjF9.tzmpYAWPv-CRx2MZXNw8XNh4kvcZpw2UDuMRZGBk1DYIg3Cz4e22p-nJkNWysktWvWu8N-s8iSv5olrb0x-7NPtzlDyAfk8whqhgp5URfKNRxeoaA2Zi2QzOMRarbfIzrSFV9deVo0rEi_Rl-ETMfySte0HmXwVk8Vu8hQ9cwIijpEgvu_nHKfKWWWPKWMswOzIYGBqvMZapi7cif9fYW8dPzP4hB8_DWy8Tm5VnEMu5l9_e9axkAfdTjGTDY231NmnOIZiS3JupyycGogeac8USiUOvwH3mUOg5FxXJhtulxKU0z_6nJiq6qH4RRGWH70QmOr7HgBwj0UlYn4ArWw')
+                xhr.setRequestHeader('accept','application/json');
+                xhr.setRequestHeader('Authorization','Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpY1NQSWQiOjYzLCJpY0FnZW50SWQiOjIwNzg2NDA1LCJzdWIiOiJ1c2VyOjIwNzg2NDA1IiwiaXNzIjoiaHR0cHM6Ly9hcGkuaW5jb250YWN0LmNvbSIsImljQlVJZCI6NDU5NTk2NCwiZ2l2ZW5fbmFtZSI6IlN0ZXBoYW5pZSIsImF1ZCI6IkRldmVsb3BlclBvcnRhbEBOSUNFaW5Db250YWN0IEluYy4iLCJuYW1lIjoic3RlcGhhbmllLmRhdmlzQGJyYW5kdGluZm8uY29tIiwidGVuYW50SWQiOiIxMWU5ZWJlNC05YmNiLThiOWUtODEwNy0wMDUwNTZhMTQwNzQiLCJmYW1pbHlfbmFtZSI6IkRhdmlzIiwiaWNDbHVzdGVySWQiOiJDMTQiLCJuYmYiOjE2NDg1MDY5MDAsImljU2NvcGUiOiIxLDIsNCw1LDYsNyw4LDEwLDEyIiwiaWF0IjoxNjQ4NTA2OTAwLCJleHAiOjE2NDg1MTA1MDB9.YwQzmPZKrQ1Nsea-PTnVShwiI0jpgMcewzn43BqHOAC68iecJEOYtr81dmwl4vO0wMZLSZR-eK8ShdkET6R39K3md0eJVSDXaJm2_Btpib0wMJ_N9MJY_Ddoizciz2Zfl0uRyLBhAPH5rFzk19Yv19TExFc0QDRSeCydzmfO8gmHomYO7TG_PiCMgygAP6n_l7vPjK6XihvSZ_BvTjpaQB-kUVGjbJ68F6ZApdbv_YpNyN5sXS8uYzIY3AFQjQNxMIYGaT_Q3YnOsVolXG1wv2T0xwblwFzeyvlUo9zltoOHujIfbjyhS3MppPJ1ql-mkR8s2nECbeu52d89eMIW3w')
             },
         });
         $.getJSON("https://api-c14.incontact.com/inContactAPI/services/v23.0/contacts/completed?startDate=2022-03-22&endDate=2022-03-23&fields=contactID%2CmasterContactID",function(data){
