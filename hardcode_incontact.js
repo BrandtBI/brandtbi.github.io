21c9eb7fb3d28108db5367ad05124abd74d0a2e2
TheSaintD <stephanie.davis@brandtinfo.com>
2022-03-28 16:10:24 -0400

    Update hardcode_incontact.js

--- hardcode_incontact.js
+++ hardcode_incontact.js
@@ -21,7 +21,7 @@
 	var tableSchema = {
 		id : "inContact",
 		alias : "Completed Contacts",
-		columns : cols,
+		columns : cols
         // incrementColumnId: "date"
 		};
     
@@ -36,7 +36,7 @@
             //url:'https://api-c14.incontact.com/inContactAPI/services/v23.0/contacts/completed?startDate=2022-03-22&endDate=2022-03-23&fields=contactID%2CmasterContactID',
             accept:'application/json',
             beforeSend:function(xhr){
-                xhr.setRequestHeader('Accept','application/json');
+                xhr.setRequestHeader('Accept','application/json'),
                 xhr.setRequestHeader('Authorization','bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpY1NQSWQiOjYzLCJpY0FnZW50SWQiOjIwNzg2NDA1LCJzdWIiOiJ1c2VyOjIwNzg2NDA1IiwiaXNzIjoiaHR0cHM6Ly9hcGkuaW5jb250YWN0LmNvbSIsImljQlVJZCI6NDU5NTk2NCwiZ2l2ZW5fbmFtZSI6IlN0ZXBoYW5pZSIsImF1ZCI6IkRldmVsb3BlclBvcnRhbEBOSUNFaW5Db250YWN0IEluYy4iLCJuYW1lIjoic3RlcGhhbmllLmRhdmlzQGJyYW5kdGluZm8uY29tIiwidGVuYW50SWQiOiIxMWU5ZWJlNC05YmNiLThiOWUtODEwNy0wMDUwNTZhMTQwNzQiLCJmYW1pbHlfbmFtZSI6IkRhdmlzIiwiaWNDbHVzdGVySWQiOiJDMTQiLCJuYmYiOjE2NDg0OTQ2MjYsImljU2NvcGUiOiIxLDIsNCw1LDYsNyw4LDEwLDEyIiwiaWF0IjoxNjQ4NDk0NjI2LCJleHAiOjE2NDg0OTgyMjZ9.aWM0VANiiLMKqM6TzVljtJQXloknIF0UNoVqMVOTTHj3So3BAx4daKEnHccTOamG-CoVfcQ6QluVaqmu-g_77GjW80HJZRXAm2vksxyRzc0A4faMUm8iK-tR1zefJQssXMX-DdXZPNUMdbwX9lgBQMrbOEhGJ9KO9yVvfEKO2zrACDHMfBZ_JA35WleNCJFdPzMFZfSRV8oo1xByu3tNByhueWyBhEdt1rip2ZHkEJMRBzRjMYzf49NcIb1hic48wr9zVVk80nttquJCB1nUh7wi5BT_KGXkS94Nk4jO-_md5nE2DidoZtmIFrMCtlE6E6OtXyaRVfs5QQGY6Q_72w')
                     },
             });
@@ -60,8 +60,7 @@
         $(document).ready(function () {
         $("#submitButton").click(function () {
             tableau.connectionName = "Completed Contacts"
-        })
-        
-        tableau.submit();
+            tableau.submit();
         });
     });
+    })();
